c38063b759dc1d694ed7c54efd139862bac2f934
MikaelaWilliman <mimawilliman@gmail.com>
2022-12-08 22:40:40 -0300

    Sintaxis

---
+++ Sintaxis.js
@@ -0,0 +1,18 @@
+// Sintaxis, Variables
+
+const list = ["Mikaela", 23, false];
+console.log(list);
+
+const nombre = ["Mikaela"];
+const edad = [23];
+const desarrollador = [false];
+
+const nacimiento = new Date("may 11 1999");
+console.log(nacimiento);
+
+const libro_favorito = {
+  titulo: "1984",
+  autor: "George Orwell",
+  fecha: "8 de junio de 1949",
+  url: "https://es.wikipedia.org/wiki/1984_(novela)",
+};
